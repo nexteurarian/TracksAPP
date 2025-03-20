bf8c20a7ad23cf49cf14802e80d4589d0409c811
Ian Quibete <imquibete01202400438@usep.edu.ph>
2025-03-20 18:53:14 +0800

    tracks @ ssc225

---
+++ IQTracksAPP/IanQTracksAPP.swiftpm/TracksApp.swift
@@ -0,0 +1,14 @@
+// Created by Ian Quibete
+
+import SwiftUI
+
+@main
+struct MyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .preferredColorScheme(.light)
+        }
+    }
+}
+
